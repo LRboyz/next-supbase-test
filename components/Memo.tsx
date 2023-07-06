e8dcba2194c2818e37e2be8f82e04eeb0fa8570e
LRboy <603552916@qq.com>
2023-07-06 09:21:00 +0800

    update

--- components/Memo.tsx
+++ components/Memo.tsx
@@ -1,5 +1,3 @@
-import { marked } from "@/utils/marked";
-
 interface Props {
   memo: Memo;
 }
@@ -8,7 +6,7 @@ const Memo: React.FC<Props> = ({ memo }) => {
   return (
     <div className="relative flex flex-col justify-start items-start shadow-md w-full p-4 pt-3 mt-2 bg-white dark:bg-zinc-700 rounded-lg border border-white dark:border-zinc-600 hover:border-gray-200 dark:hover:border-zinc-600;r">
       <b>{memo.id}</b>
-      <div>{marked(memo.content)}</div>
+      <div>{memo.content}</div>
     </div>
   );
 };
